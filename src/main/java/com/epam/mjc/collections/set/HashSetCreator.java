e422e0177ad4865077e478b13b3062a52070980f
Jahongir04 <jahongirachilov004@gmail.com>
2024-01-05 15:39:59 +0500

    fixed all

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,24 +5,26 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
-        HashSet<Integer> expectedHashSet = new HashSet<>();
-        int count=0;
+        HashSet<Integer>hashSet=new HashSet<>();
         for (int i = 0; i < sourceList.size(); i++) {
-            if (sourceList.get(i) % 2 == 0) {
-                count++;
-                if (count==0){
-                    expectedHashSet.add(sourceList.get(i));
-                }
-                int number=sourceList.get(i);
-                for (int j = 0; j < count; j++) {
-                    expectedHashSet.add(number / 2);
-                }
-                --i;
-            } else {
-                expectedHashSet.add(sourceList.get(i));
-                expectedHashSet.add(sourceList.get(i) * 2);
+            if (sourceList.get(i)%2==0){
+                odd(hashSet,sourceList.get(i));
+            }else {
+                hashSet.add(sourceList.get(i));
+                hashSet.add(sourceList.get(i)*2);
             }
         }
-        return expectedHashSet;
+        return hashSet;
+    }
+    public static void odd(HashSet<Integer>hashSet,int number){
+        while (true) {
+            if (number % 2 == 0) {
+                hashSet.add(number);
+                number=number/2;
+            }else {
+                hashSet.add(number);
+                break;
+            }
+        }
     }
 }
